6eb909a6e0ab43eda82133cea5808112ea2c7fac
Deorcan <54554688+Deorcan@users.noreply.github.com>
2021-09-13 18:37:29 -0700

    Update 3V.js

--- Test Drive/3V.js
+++ Test Drive/3V.js
@@ -57,7 +57,7 @@ function moveUp(){
 		return
 	} else {
 		let pos = parseInt(TOP)
-		pos -=0.28
+		pos -=0.01
 
 		ship.style.top = `${pos}%`
 	}
@@ -69,7 +69,7 @@ function moveDown(){
 		return
 	} else {
 		let pos = parseInt(TOP)
-		pos +=0.28
+		pos +=0.01
 
 		ship.style.top = `${pos}%`
 	}
@@ -81,7 +81,7 @@ function moveLeft(){
 		return
 	} else {
 		let pos = parseInt(LEFT)
-		pos -=0.584
+		pos -=0.01
 	
 		ship.style.left = `${pos}%`
 	}
@@ -93,7 +93,7 @@ function moveRight(){
 		return
 	} else {
 		let pos = parseInt(LEFT)
-		pos +=0.584
+		pos +=0.01
 	
 		ship.style.left = `${pos}%`
 	}
