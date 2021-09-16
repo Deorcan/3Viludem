9f0b52a29850cc96c92ef112b563677f4cdf9c81
Deorcan <54554688+Deorcan@users.noreply.github.com>
2021-09-15 19:33:05 -0700

    Update 3V.js

--- Test Drive/3V.js
+++ Test Drive/3V.js
@@ -89,7 +89,7 @@ function moveLeft(){
 
 function moveRight(){
 	let LEFT = window.getComputedStyle(ship).getPropertyValue('left')
-	if (ship.style.left >= "900.188%"){
+	if (ship.style.left >= "9000.188%"){
 		return
 	} else {
 		let pos = parseInt(LEFT)
@@ -165,7 +165,7 @@ function fire(){
 function moveEnemy(enemy){
 	let interval = setInterval(() => {
 		let X = parseInt(window.getComputedStyle(enemy).getPropertyValue('left'))
-		if (X <= 160){
+		if (X <= 140){
 			if (Array.from(enemy.classList).includes("dead")){
 				let Whoosh = new Audio ('Test Drive/Whoosh.mp3')
 				Whoosh.play()
@@ -195,7 +195,7 @@ if (TF === false){
 	newenemy.src = sprite
 	newenemy.classList.add('enemy')
 	newenemy.classList.add('transition')
-	newenemy.style.left = '700px'
+	newenemy.style.left = '900px'
 	newenemy.style.top = `${Math.floor(Math.random()* 530) + 50 }px`
 	gamescreen.appendChild(newenemy)
 	moveEnemy(newenemy)
