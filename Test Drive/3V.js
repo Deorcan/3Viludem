1beea7e0bff0bbb65730434cd77d3ea3b0310dc4
Deorcan <54554688+Deorcan@users.noreply.github.com>
2021-09-21 14:51:17 -0700

    Update 3V.js

--- Test Drive/3V.js
+++ Test Drive/3V.js
@@ -89,7 +89,7 @@ function moveLeft(){
 
 function moveRight(){
 	let LEFT = window.getComputedStyle(ship).getPropertyValue('left')
-	if (ship.style.left >= "591.09589%"){
+	if (ship.style.left >= "991.09589%"){
 		return
 	} else {
 		let pos = parseInt(LEFT)
