9185a107d87203f7a9b491467a6218bc97c45518
Deorcan <54554688+Deorcan@users.noreply.github.com>
2021-09-25 17:17:13 -0700

    Update 3V.js

--- Test Drive/3V.js
+++ Test Drive/3V.js
@@ -198,7 +198,7 @@ if (TF === false){
 	newenemy.classList.add('enemy')
 	newenemy.classList.add('transition')
 	newenemy.style.left = "94%"
-	newenemy.style.top = `${Math.floor(Math.random()* 530) + 20 }px`
+	newenemy.style.top = `${Math.floor(Math.random()* 530) + 50 }%`
 	gamescreen.appendChild(newenemy)
 	moveEnemy(newenemy)
 }
