ca49671c1de74e35bb4412c1585fe3122954259b
Deorcan <54554688+Deorcan@users.noreply.github.com>
2021-10-03 19:35:10 -0700

    Update 3V.js

--- Test Drive/3V.js
+++ Test Drive/3V.js
@@ -56,7 +56,7 @@ function gameover(){
 
 function moveUp(){
 	let TOP = window.getComputedStyle(ship).getPropertyValue('top')
-	if (ship.style.top < "3%"){
+	if (ship.style.top <= Image.height-Image.height){
 		return
 	} else {
 		let pos = parseInt(TOP)
@@ -68,7 +68,7 @@ function moveUp(){
 
 function moveDown(){
 	let TOP = window.getComputedStyle(ship).getPropertyValue('top')
-	if (ship.style.top > "94%"){
+	if (ship.style.top >= Image.height){
 		return
 	} else {
 		let pos = parseInt(TOP)
@@ -80,7 +80,7 @@ function moveDown(){
 
 function moveLeft(){
 	let LEFT = window.getComputedStyle(ship).getPropertyValue('left')
-	if (ship.style.left < "64%"){
+	if (ship.style.left <= Image.width-Image.width){
 		return
 	} else {
 		let pos = parseInt(LEFT)
@@ -92,7 +92,7 @@ function moveLeft(){
 
 function moveRight(){
 	let LEFT = window.getComputedStyle(ship).getPropertyValue('left')
-	if (ship.style.left > "18%"){
+	if (ship.style.left >= Image.width){
 		return
 	} else {
 		let pos = parseInt(LEFT)
