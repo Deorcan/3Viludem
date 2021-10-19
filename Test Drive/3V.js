129a567b220de67d2f384e9981ca20619199989c
Deorcan <54554688+Deorcan@users.noreply.github.com>
2021-10-18 17:03:42 -0700

    Update 3V.js

--- Test Drive/3V.js
+++ Test Drive/3V.js
@@ -57,7 +57,7 @@ function gameover(){
 
 function moveUp(){
 	let TOP = window.getComputedStyle(ship).getPropertyValue('top')
-	if (ship.style.top <= "0vh"){
+	if (ship.style.top < "0vh"){
 		return
 	} else {
 		let pos = parseInt(TOP)
