4f2d9b8ee7ba094fdd2511f43a4864c9e9160c38
Deorcan <54554688+Deorcan@users.noreply.github.com>
2021-12-20 14:43:59 -0800

    Update 3V.js

--- Test Drive/3V.js
+++ Test Drive/3V.js
@@ -51,7 +51,7 @@ Image.onmousemove = function() {OnMouse()}
 Image.onmouseout = function() {MouseOff()}
 
 //Para.addListener(function(p) {if (p.style.marginTop === !PG) {  }})
-Para.style.marginTop = PG
+Para.style.top = PG
 const userAgent = navigator.userAgent.toLowerCase();
 var isMobile = /iPhone|Android|BlackBerry|BB|Nokia/i.test(navigator.userAgent);
 const isTablet = /(ipad|tablet|(android(?!.*mobile))|(windows(?!.*phone)(.*touch))|kindle|playbook|silk|(puffin(?!.*(IP|AP|WP))))/.test(userAgent);
