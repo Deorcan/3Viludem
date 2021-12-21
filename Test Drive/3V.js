c1dc63fc042796d0a2fea073e98fbceba1f27438
Deorcan <54554688+Deorcan@users.noreply.github.com>
2021-12-21 15:07:47 -0800

    Update 3V.js

--- Test Drive/3V.js
+++ Test Drive/3V.js
@@ -68,9 +68,9 @@ function isTabletOrMobile(){
 		Image.style.right = "3vw"
 		Image.style.top = "25vh"
 		Image.style.maxWidth = "94%"
-		//Para.style.left = "0vw"
-		//Para.style.right = "0vw"
-		Para.addListener(function(p) {if ((land.matches && isMobile) || (land.matches && isTablet)) { p.style.top = '120vh' }else{p.style.top = '110vh'}})
+		Para.style.marginLeft = "0vw"
+		Para.style.marginRight = "0vw"
+		//Para.addListener(function(p) {if ((land.matches && isMobile) || (land.matches && isTablet)) { p.style.top = '120vh' }else{p.style.top = '110vh'}})
 		Control.style.display = "none"
 		text2.style.marginTop = "5vh"
 		
