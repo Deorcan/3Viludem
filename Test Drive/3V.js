a4f3dfaeefc6e0b2e34aa1fdd30350dee6159613
Deorcan <54554688+Deorcan@users.noreply.github.com>
2022-01-07 16:12:30 -0800

    Update 3V.js

--- Test Drive/3V.js
+++ Test Drive/3V.js
@@ -440,8 +440,8 @@ function OnTouch(){
 	}
 	
 
-	document.addListener('change', PortRotate)
-	document.addListener('change', LandRotate)
+	screen.addListener('change', PortRotate)
+	screen.addListener('change', LandRotate)
 	
 	TiltMode.ontouchstart = function (){
 		TiltMode.style.border = "solid yellow 2px"
