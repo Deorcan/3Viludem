d8f404703857ff088c80af273ef7c0370928429f
Deorcan <54554688+Deorcan@users.noreply.github.com>
2022-01-08 16:49:16 -0800

    Update 3V.js

--- Test Drive/3V.js
+++ Test Drive/3V.js
@@ -440,8 +440,8 @@ function OnTouch(){
 	}
 	
 
-	port.addListener(function(){ PortRotate() })
-	land.addListener(function(){ LandRotate() })
+	port.addListener('event', function(){ PortRotate() })
+	land.addListener('event', function(){ LandRotate() })
 	
 	TiltMode.ontouchstart = function (){
 		TiltMode.style.border = "solid yellow 2px"
@@ -480,8 +480,8 @@ function OnTouch(){
 			Menu.style.display = 'none'
 			//port.removeListener(function(){ PortRotate() })
 			//land.removeListener(function(){ LandRotate() })
-			port.removeListener(,(function(){ PortRotate() })
-			land.removeListener(,(function(){ LandRotate() })
+			port.removeListener('event', function(){ PortRotate() })
+			land.removeListener'event', (function(){ LandRotate() })
 			Image.style.top = "0vh"
 			Image.style.left = "0vw"
 			Image.style.maxWidth = `${window.innerWidth}px`
