730a54f0a4c12456306f8de59df7082922223992
Deorcan <54554688+Deorcan@users.noreply.github.com>
2022-01-19 12:19:19 -0800

    Update 3V.js

--- Test Drive/3V.js
+++ Test Drive/3V.js
@@ -553,14 +553,20 @@ function OnTouch(){
 		}
 	}
 	
-	
 	CloseButton.onclick = function (){ Menu.style.display = 'none'}
+	
 	if (Display === 'block' && land.matches && Mode === 0){
 		if(!Menu.onclick){ Menu.style.display = 'none'}	
 	}else if (Display === 'none' && land.matches && Mode === 0){
 		if(!Menu.onclick){Menu.style.display = 'block'}
 	}
-	
+	PauseButton.onclick = function (){
+		if (PauseButton.src == 'Test%20Drive/PauseButton.png'){
+			PauseButton.src = 'Test%20Drive/PauseButton1.png'
+		}else if (PauseButton.src == 'Test%20Drive/PauseButton1.png'){
+			PauseButton.src = 'Test%20Drive/PauseButton.png'
+		}
+	}
 	
 }
 
