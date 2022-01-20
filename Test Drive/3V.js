2a5b9e9e3c42b9c7ab3ff558908f43e333df18cb
Deorcan <54554688+Deorcan@users.noreply.github.com>
2022-01-19 16:00:38 -0800

    Update 3V.js

--- Test Drive/3V.js
+++ Test Drive/3V.js
@@ -441,6 +441,7 @@ PlayTouch = function(){
 	Image.style.maxHeight = `${window.innerHeight-5}px`
 	landX = `${window.innerWidth-5}px`
 	landY = `${window.innerHeight-5}px`
+	Pause.style.fontSize = '225%'
 	
 	PauseButton.style.display = 'block'
 	ScoreT2.style.display = 'block'
