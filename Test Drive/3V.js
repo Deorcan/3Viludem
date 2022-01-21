035e47beac7784bf8fa411a318f712f8f1d083d8
Deorcan <54554688+Deorcan@users.noreply.github.com>
2022-01-21 12:26:01 -0800

    Update 3V.js

--- Test Drive/3V.js
+++ Test Drive/3V.js
@@ -442,7 +442,7 @@ PlayTouch = function(){
 	landX = `${window.innerWidth-5}px`
 	landY = `${window.innerHeight-5}px`
 	Pause.style.fontSize = '225%'
-	Pause.style.marginTop = '-50%'
+	Pause.style.marginTop = '-35%'
 	
 	PauseButton.style.display = 'block'
 	ScoreT2.style.display = 'block'
