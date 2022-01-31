3897074309766f61a6edc7f9b9b754463cff90a4
Deorcan <54554688+Deorcan@users.noreply.github.com>
2022-01-31 12:03:19 -0800

    Update 3V.js

--- Test Drive/3V.js
+++ Test Drive/3V.js
@@ -183,7 +183,7 @@ function RESTART(){
 		ControlMenu.style.display = 'none'
 		starting = 0
 		PlayButton.src = 'Test%20Drive/Playbutton.PNG'
-		Menu.style.marginTop = '10%'
+		Menu.style.marginTop = '5%'
 		TouchStart.style.top = '40%'
 		Para.style.display = 'none'
 		TF = false
@@ -494,7 +494,7 @@ Landing = function (){
 PlayTouch = function(){
 	isTabletOrMobile()
 	Menu.style.display = 'none'
-	//Menu.style.marginTop = '-27%'
+	Menu.style.marginTop = '-27%'
 	PlayButton.src = 'Test%20Drive/Donebutton.PNG'
 	Image.style.top = "0vh"
 	Image.style.left = "0vw"
