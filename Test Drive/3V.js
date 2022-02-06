75d4c6a3b6ce7f0bdbf895788436674a6ab434ad
Deorcan <54554688+Deorcan@users.noreply.github.com>
2022-02-05 17:05:58 -0800

    Update 3V.js

--- Test Drive/3V.js
+++ Test Drive/3V.js
@@ -521,7 +521,7 @@ Landing = function (){
 			if (Displaytouch === 'block' || Displaygo === 'block'){Menu.style.display = 'none'}
 			GameOver.style.marginTop = '25%'
 			setTimeout(function(){ window.scrollTo(0,1), 0})
-			var Enemies = document.querySelectorAll(".enemy")
+			let Enemies = document.querySelectorAll(".enemy")
 			Enemies.forEach(e => e.style.display = 'block')
 			Lasers.style.display = 'block'
 		}
