2b8bce91644d30e98094f984eb89c9bae999bc9c
Deorcan <54554688+Deorcan@users.noreply.github.com>
2022-02-12 16:03:41 -0800

    Update 3V.js

--- Test Drive/3V.js
+++ Test Drive/3V.js
@@ -840,6 +840,7 @@ function playgame(){
 			if (Mode === 1){}
 			else if(Mode === 2){
 				Image.onpointermove = function(){
+					event.preventDefault()
 					var X = event.touches[0].clientX
 					var Y = event.touches[0].clientY
 					ship.style.display = 'block'
