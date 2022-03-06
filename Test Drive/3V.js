bf6065ddd71d4c59df384fa9c493bca4d0770859
Deorcan <54554688+Deorcan@users.noreply.github.com>
2022-03-06 13:19:08 -0800

    Update 3V.js

--- Test Drive/3V.js
+++ Test Drive/3V.js
@@ -299,10 +299,10 @@ function CreateLaser(){
 		var X = event.clientX
 		var Y = event.clientY
 	}else if (IfTouch === true && Mode === 4){
-		if (event.touches.length === 0){
+		if (event.touchList.length === 0){
 			var X = event.touches[0].clientX
 			var Y = event.touches[0].clientY
-	}else if (event.touches.length === 1){
+	}else if (event.touchList.length === 1){
 			var X = event.touches[1].clientX
 			var Y = event.touches[1].clientY
 	}
@@ -442,10 +442,10 @@ function movingtouch(){
 	let LEFTwidth = window.getComputedStyle(DPadLeft).getPropertyValue('width')
 	let LEFTright = window.getComputedStyle(DPadRight).getPropertyValue('left')
 	
-	if (event.touches.length === 0){
+	if (event.touchList.length === 0){
 		var X = event.touches[0].clientX
 		var Y = event.touches[0].clientY
-	}else if (event.touches.length === 1){
+	}else if (event.touchList.length === 1){
 		var X = event.touches[1].clientX
 		var Y = event.touches[1].clientY
 	}
