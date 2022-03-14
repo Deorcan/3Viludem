9fc4d07464ee49890316fe9c9894ae325445051d
Deorcan <54554688+Deorcan@users.noreply.github.com>
2022-03-13 20:04:39 -0700

    Update 3V.js

--- Test Drive/3V.js
+++ Test Drive/3V.js
@@ -696,7 +696,7 @@ Landing = function (){
 			Image.style.left = "0vw"
 			Image.style.maxWidth = landX
 			Image.style.maxHeight = landY			
-Image.ontouchstart = function(){Menu.style.display = 'block'}
+
 			PauseButton.style.display = 'block'
 			ScoreT2.style.display = 'block'
 			HighScoreT2.style.display = 'block'
@@ -732,6 +732,7 @@ Image.ontouchstart = function(){Menu.style.display = 'block'}
 			var Enemies = document.querySelectorAll(".enemy")
 			Enemies.forEach(e => e.style.display = 'block')
 			Lasers.style.display = 'block'
+			Image.ontouchstart = function(){Menu.style.display = 'block'}
 		}
 	}
 }
