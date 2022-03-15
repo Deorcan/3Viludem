72bec6a99c14dfaf8e91ea14346f05cfe79bf391
Deorcan <54554688+Deorcan@users.noreply.github.com>
2022-03-15 16:00:26 -0700

    Update 3V.js

--- Test Drive/3V.js
+++ Test Drive/3V.js
@@ -907,7 +907,12 @@ function OnTouch(){
 		PlayButton.onclick = function (){
 		
 			Mode = 1
-			PlayTouch()
+			if (typeof DeviceOrientationEvent.requestPermission === 'function') {
+      				DeviceOrientationEvent.requestPermission().then(permissionState => {
+		  			if (permissionState === 'granted') {
+           					 window.addEventListener('deviceorientation', () => {})
+         			 	}}).catch(console.error)} else {/*handle regular non iOS 13+ devices*/}	
+			//PlayTouch()
 		}
 	}
 	PenMode.onclick = function (){
