8107fe94faa5655f16a7dcefa0fce3a079f1c98b
Deorcan <54554688+Deorcan@users.noreply.github.com>
2022-03-15 12:13:42 -0700

    Update 3V.js

--- Test Drive/3V.js
+++ Test Drive/3V.js
@@ -706,7 +706,7 @@ Landing = function (){
 			Para.style.display = 'none'
 			Menu.style.display = 'none'
 			PauseButton.src = "Test%20Drive/PauseButton1.png"
-			if (starting === -1 && TF === false){
+			if (starting === -1){
 				Pause.style.display = 'block'
 				Start.style.display = 'block'
 				Restart.style.display = 'block'
