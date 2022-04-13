326cb1bcda7c7a2f0142c61485d834476c150b6c
Deorcan <54554688+Deorcan@users.noreply.github.com>
2022-04-13 13:40:21 -0700

    Update 3V.js

--- Test Drive/3V.js
+++ Test Drive/3V.js
@@ -106,9 +106,6 @@ function isTabletOrMobile(){
 		Touchscreen.style.marginLeft = "12%"
 		body.onload = function(){
 			Cover.style.marginLeft = `${-(window.innerWidth+5)}px`;
-			 var a = document.getElementById("a");
-			
-			 a.innerHTML="devicePixelRatio is : "+window.devicePixelRatio; 
 			Page.style.transformOrigin = "left";
 			Page.style.transition = "transform 10s";
 			Page.style.transformStyle = "preserve-3d";
