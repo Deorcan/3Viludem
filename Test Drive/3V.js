3ac64379bd22e8c88bc12b6d24821b5daae8cb6f
Deorcan <54554688+Deorcan@users.noreply.github.com>
2022-07-05 09:17:36 -0700

    Update 3V.js

--- Test Drive/3V.js
+++ Test Drive/3V.js
@@ -745,7 +745,7 @@ function Porting(){
 		Image.style.top = "25vh"
 		Image.style.left = "3vw"
 		Image.style.maxWidth = "94%"
-		Image.style.maxHeight = "94vh"
+		Image.style.maxHeight = "94%"
 				
 		PauseButton.style.display = 'none'
 		ScoreT2.style.display = 'none'
