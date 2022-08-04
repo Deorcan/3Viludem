dc2c11ea36ec94f28232da7895e85b5a0b0af758
Deorcan <54554688+Deorcan@users.noreply.github.com>
2022-08-04 13:04:07 -0400

    Update 3V.js

--- Test Drive/3V.js
+++ Test Drive/3V.js
@@ -714,13 +714,13 @@ function MouseOff(){
 function Porting(){
 	if (port.matches){
 		
-		Icon.style.marginLeft = "-60vw";
+		Icon.style.marginLeft = "-20vw";
 		Icon.style.marginTop = "20vh";
 		Icon.style.height = "20%";
 		Icon.style.width = "20%";
 		Title.style.marginTop = "20vh";
 		Title.style.fontSize = "200%";
-		Title.style.marginLeft = "-65vw";
+		Title.style.marginLeft = "-25vw";
 		Cover.style.padding = "50%";
 		body.onload = function(){
 			body.style.transition = "5s";
